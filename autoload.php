d4f699ec09507df3e231e86e4b4e98308ebcc676
Kjell-Inge Gustafsson <ical@kigkonsult.se>
2021-10-25 18:34:48 +0200

    iCalcreator-2.40.1

--- autoload.php
+++ autoload.php
@@ -36,7 +36,7 @@
 /**
  *         Do NOT alter or remove the constant!!
  */
-define( 'ICALCREATOR_VERSION', 'iCalcreator 2.40' );
+define( 'ICALCREATOR_VERSION', 'iCalcreator 2.40.1' );
 /**
  * load iCalcreator src and support classes and Traits
  */
